0bdfc3ae0d42336708e864ed73fe103aec4da2ef
VladissGernet <skeimorr@gmail.com>
2025-05-27 14:04:12 +0400

    feat: подготовка второго задания.

--- src/components/blocks/start/general.stories.jsx
+++ src/components/blocks/start/general.stories.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
-import { General1 } from "./general-1";
-
 import { StyledPage } from "./styled";
+
+import { General1 } from "./general-1";
+import { General2 } from "./general-2";
 
 export const General1Start = () => (
   <StyledPage>
     <General1 />
+  </StyledPage>
+);
+
+export const General2Start = () => (
+  <StyledPage>
+    <General2 />
   </StyledPage>
 );
 
