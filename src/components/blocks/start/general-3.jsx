a49d254bace3d2a758684d4996a9aa1074e3b47a
VladissGernet <skeimorr@gmail.com>
2025-05-30 21:28:40 +0400

    feat: упражнение закончено

--- src/components/blocks/start/general-3.jsx
+++ src/components/blocks/start/general-3.jsx
@@ -7,10 +7,6 @@ const General3 = () => {
     чисел найти то, которое отличается по чётности от остальных, и вернуть его позицию.
   */
 
-  const numbers1 = "2 4 7 8 10";
-  const numbers2 = "1 2 1 1";
-  const numbers3 = "1 1 1 2";
-
   /*
     План решения задачи:
     1. На первых трех числах определить, четный массив или нет.
@@ -48,56 +44,84 @@ const General3 = () => {
   };
 
   // Второе мое решение с помощью побитовых операций.
-  const iqTestSolution2 = (numbers) => {
-    const arr = numbers.split(" ").map((number) => Number(number).toString(2));
+  // const iqTestSolution2 = (numbers) => {
+  //   const arr = numbers.split(" ").map((number) => Number(number).toString(2));
 
-    // По умолчанию ставлю: четный ? - нет.
-    let isEven = false;
-    // Проверяем сумму последних чисел для проверки четности массива.
-    let sumOfLastNumbers = 0;
-    // На случай, если в первых трех числах есть одно отличающиеся четностью.
-    const numberIndexObject = {};
+  //   // По умолчанию ставлю: четный ? - нет.
+  //   let isEven = false;
+  //   // Проверяем сумму последних чисел для проверки четности массива.
+  //   let sumOfLastNumbers = 0;
+  //   // На случай, если в первых трех числах есть одно отличающиеся четностью.
+  //   const numberIndexObject = {};
 
-    for (let i = 0; i < arr.length; i++) {
-      if (i < 3) {
-        const lastNumber = Number(arr[i][arr[i].length - 1]);
-        numberIndexObject[lastNumber === 0 ? "even" : "odd"] = i;
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (i < 3) {
+  //       const lastNumber = Number(arr[i][arr[i].length - 1]);
+  //       numberIndexObject[lastNumber === 0 ? "even" : "odd"] = i;
 
-        sumOfLastNumbers += lastNumber;
-      } else if (i === 3) {
-        // Определяем четность массива.
-        isEven = sumOfLastNumbers < 2 ? true : false;
+  //       sumOfLastNumbers += lastNumber;
+  //     } else if (i === 3) {
+  //       // Определяем четность массива.
+  //       isEven = sumOfLastNumbers < 2 ? true : false;
 
-        /*
-          Если среди первых трех чисел есть одно число, отличающегося четностью, то сделать return этого отличающигося четностью числа.
-          Если массив четный, то возвращаем индекс нечетного числа, если нечетный — четного.
-        */
+  //       /*
+  //         Если среди первых трех чисел есть одно число, отличающегося четностью, то сделать return этого отличающигося четностью числа.
+  //         Если массив четный, то возвращаем индекс нечетного числа, если нечетный — четного.
+  //       */
 
-        if (sumOfLastNumbers !== 0 && sumOfLastNumbers !== 3) {
-          return isEven
-            ? numberIndexObject["odd"] + 1
-            : numberIndexObject["even"] + 1;
-        }
+  //       if (sumOfLastNumbers !== 0 && sumOfLastNumbers !== 3) {
+  //         return isEven
+  //           ? numberIndexObject["odd"] + 1
+  //           : numberIndexObject["even"] + 1;
+  //       }
 
-        // Если первые три числа с одинаковой четностью, то цикл продложать до тех пор, пока не найдется отличающеся четностью число.
-        const expectedBit = isEven ? "0" : "1";
-        if (arr[i][arr[i].length - 1] !== expectedBit) {
-          return i + 1;
-        }
-      } else {
-        // Продолжаем искать число по массиву отличающееся четностью.
-        const expectedBit = isEven ? "0" : "1";
-        if (arr[i][arr[i].length - 1] !== expectedBit) {
-          return i + 1;
-        }
-      }
-    }
+  //       // Если первые три числа с одинаковой четностью, то цикл продложать до тех пор, пока не найдется отличающеся четностью число.
+  //       const expectedBit = isEven ? "0" : "1";
+  //       if (arr[i][arr[i].length - 1] !== expectedBit) {
+  //         return i + 1;
+  //       }
+  //     } else {
+  //       // Продолжаем искать число по массиву отличающееся четностью.
+  //       const expectedBit = isEven ? "0" : "1";
+  //       if (arr[i][arr[i].length - 1] !== expectedBit) {
+  //         return i + 1;
+  //       }
+  //     }
+  //   }
 
-    return undefined; // Если четность не определена, возвращаем undefined.
-  };
+  //   return undefined; // Если четность не определена, возвращаем undefined.
+  // };
+
+  /*
+    iqTestSolution1: O(n) — лучшее решение по скорости и памяти
+    iqTestSolution2: O(n * log m) — более затратное из-за преобразований в двоичный формат
+  */
 
   // test(iqTest("2 4 7 8 10"), 3);
   // test(iqTest("1 2 1 1"), 2);
+
+  /*
+  Решение задач из гайда.
+  function iqTest(numbers) {
+    const parsedNumbers = numbers.split(" ").map((el) => parseInt(el));
+
+    const odd = parsedNumbers.filter((el) => el % 2 === 1);
+    const even = parsedNumbers.filter((el) => el % 2 === 0);
+
+    return odd.length < even.length
+      ? parsedNumbers.indexOf(odd[0]) + 1
+      : parsedNumbers.indexOf(even[0]) + 1;
+  }
+
+  function iqTest(numbers) {
+    const remainders = numbers.split(" ").map((x) => x % 2);
+
+    const sum = remainders.reduce((a, b) => a + b);
+    const target = sum > 1 ? 0 : 1;
+
+    return remainders.indexOf(target) + 1;
+  }
+*/
 
   return (
     <div>
@@ -107,9 +131,9 @@ const General3 = () => {
         тесте iq — из списка чисел найти то, которое отличается по чётности от
         остальных, и вернуть его позицию.
       </p>
-      <Tester result={iqTestSolution2("2 4 7 8 10")} answer={3} />
-      <Tester result={iqTestSolution2("1 1 1 1 1 2 1")} answer={6} />
-      <Tester result={iqTestSolution2("1 2 1 1")} answer={2} />
+      <Tester result={iqTestSolution1("2 4 7 8 10")} answer={3} />
+      <Tester result={iqTestSolution1("1 1 1 1 1 2 1")} answer={6} />
+      <Tester result={iqTestSolution1("1 2 1 1")} answer={2} />
     </div>
   );
 };
