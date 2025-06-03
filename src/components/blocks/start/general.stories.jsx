9162c6c336cca74dfa3839bcd365c5e4b54802af
VladissGernet <skeimorr@gmail.com>
2025-06-03 15:02:07 +0400

    feat: подготовка задачи

--- src/components/blocks/start/general.stories.jsx
+++ src/components/blocks/start/general.stories.jsx
@@ -6,6 +6,7 @@ import { General1 } from "./general-1";
 import { General2 } from "./general-2";
 import { General3 } from "./general-3";
 import { General4 } from "./general-4";
+import { General5 } from "./general-5";
 
 export const General1Start = () => (
   <StyledPage>
@@ -26,6 +27,11 @@ export const General3Start = () => (
 export const General4Start = () => (
   <StyledPage>
     <General4 />
+  </StyledPage>
+);
+export const General5Start = () => (
+  <StyledPage>
+    <General5 />
   </StyledPage>
 );
 
