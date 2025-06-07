03f8b515cf4d3888b6119d5cde382d0f3e6584f8
VladissGernet <skeimorr@gmail.com>
2025-06-07 14:32:40 +0400

    feat: подготовка данных

--- src/components/blocks/search/search.stories.jsx
+++ src/components/blocks/search/search.stories.jsx
@@ -3,10 +3,17 @@ import React from "react";
 import { StyledPage } from "/src/components/blocks/start/styled";
 
 import { Search1 } from "./search-1";
+import { Search2 } from "./search-2";
 
 export const SearchBinary = () => (
   <StyledPage>
     <Search1 />
+  </StyledPage>
+);
+
+export const SearchHometask = () => (
+  <StyledPage>
+    <Search2 />
   </StyledPage>
 );
 
