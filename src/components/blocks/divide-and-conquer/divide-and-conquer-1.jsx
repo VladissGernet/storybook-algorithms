69286caca45aac735e7027324d254d80928f6f7a
VladissGernet <skeimorr@gmail.com>
2025-06-11 20:37:44 +0400

    feat: подготовил файлы для темы 4

---
+++ src/components/blocks/divide-and-conquer/divide-and-conquer-1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DivideAndConquer1 = () => {
+  return (
+    <div>
+      <h1>Пример</h1>
+    </div>
+  );
+};
+
+export { DivideAndConquer1 };
---
+++ src/components/blocks/divide-and-conquer/divide-and-conquer.stories.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { StyledPage } from "/src/components/blocks/start/styled";
+
+import { DivideAndConquer1 } from "./divide-and-conquer-1";
+
+export const SearchBinary = () => (
+  <StyledPage>
+    <DivideAndConquer1 />
+  </StyledPage>
+);
+
+export default {
+  title: "Разделяй и властвуй.",
+};
