1f4519ac2e6e12b8220700cc10e08991af01f4b8
VladissGernet <skeimorr@gmail.com>
2025-06-24 13:52:01 +0400

    feat: подготовка к решению задачи фибоначи

--- src/components/blocks/divide-and-conquer/divide-and-conquer.stories.jsx
+++ src/components/blocks/divide-and-conquer/divide-and-conquer.stories.jsx
@@ -3,10 +3,17 @@ import React from "react";
 import { StyledPage } from "/src/components/blocks/start/styled";
 
 import { DivideAndConquer1 } from "./divide-and-conquer-1";
+import { DivideAndConquer2 } from "./divide-and-conquer-2";
 
 export const SearchBinary = () => (
   <StyledPage>
     <DivideAndConquer1 />
+  </StyledPage>
+);
+
+export const fibonacciNumbers = () => (
+  <StyledPage>
+    <DivideAndConquer2 />
   </StyledPage>
 );
 
