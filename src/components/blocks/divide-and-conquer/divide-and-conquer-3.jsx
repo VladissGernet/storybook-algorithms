dba73e2e21f77c7314bac8560a83806ed6a3f6de
VladissGernet <skeimorr@gmail.com>
2025-06-26 12:39:20 +0400

    feat: закончил с решением задачи бинарного рекурсивного поиска.

--- src/components/blocks/divide-and-conquer/divide-and-conquer-3.jsx
+++ src/components/blocks/divide-and-conquer/divide-and-conquer-3.jsx
@@ -15,13 +15,28 @@ const DivideAndConquer3 = () => {
       binarySearch([1, 2, 3, 5, 6], 4) // false
   */
 
-  const binarySearch = (arr, value) => {
-    let left = 0;
-    let right = arr.length - 1;
+  const binarySearch = (arr, value, left = 0, right = arr.length - 1) => {
+    // Базовый случай.
+    if (left > right) {
+      // Если число не найдено, то вернуть false.
+      return false;
+    } else {
+      // Иначе находим текущую середину и округляем.
+      const mid = Math.floor((left + right) / 2);
 
-    console.log(arr);
+      if (arr[mid] === value) {
+        // Если число найдено, то возвращаем true.
+        return true;
+      } else if (arr[mid] < value) {
+        left = mid + 1;
+        // Если число из середины текущего расчета в массиве МЕНЬШЕ искомого value, то убираем левую часть.
+      } else if (arr[mid] > value) {
+        right = mid - 1;
+        // Если число из середины текущего расчета в массиве БОЛЬШЕ искомого value, то убираем правую часть.
+      }
+    }
 
-    return 0;
+    return binarySearch(arr, value, left, right);
   };
 
   return (
@@ -32,10 +47,10 @@ const DivideAndConquer3 = () => {
         элемента, а лишь говорить, присутствует элемент или нет (этакий
         оптимизированный includes на сортированном массиве).
       </p>
-      {/* <Tester result={binarySearch([], 3)} answer={false} /> */}
-      {/* <Tester result={binarySearch([3], 3)} answer /> */}
+      <Tester result={binarySearch([], 3)} answer={false} />
+      <Tester result={binarySearch([3], 3)} answer />
       <Tester result={binarySearch([1, 2, 3, 4, 5], 4)} answer />
-      {/* <Tester result={binarySearch([1, 2, 3, 5, 6], 4)} answer={false} /> */}
+      <Tester result={binarySearch([1, 2, 3, 5, 6], 4)} answer={false} />
     </div>
   );
 };
