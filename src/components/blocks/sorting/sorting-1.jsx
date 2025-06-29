da1efedd06f3cae467662ff9641d9421bb658b26
VladissGernet <skeimorr@gmail.com>
2025-06-29 18:44:55 +0400

    feat: заменил на деструктуризацию

--- src/components/blocks/sorting/sorting-1.jsx
+++ src/components/blocks/sorting/sorting-1.jsx
@@ -10,10 +10,7 @@ const bubbleSort = (arr) => {
     let isArrChanged = false;
     for (let i = 0; i < arr.length - 1; i++) {
       if (arr[i] > arr[i + 1]) {
-        const newValue1 = arr[i];
-        const newValue2 = arr[i + 1];
-        arr[i] = newValue2;
-        arr[i + 1] = newValue1;
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
         isArrChanged = true;
       }
     }
