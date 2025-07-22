7ba1ebb79a924d1f566872fd3cb988877caf405f
VladissGernet <skeimorr@gmail.com>
2025-07-22 06:16:25 +0400

    feat: хеш таблицы первый пример

---
+++ src/components/blocks/cache/cache.stories.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { StyledPage } from "/src/components/blocks/start/styled";
+
+import { Cache1 } from "./cache-1";
+
+export const mapCaching = () => (
+  <StyledPage>
+    <Cache1 />
+  </StyledPage>
+);
+
+export default {
+  title: "Хеш-таблицы",
+};
