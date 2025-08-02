f67e686e1204f6628c9e98e70509cba0d0f3dd82
VladissGernet <skeimorr@gmail.com>
2025-08-02 20:31:05 +0400

    feat: добавил решение из гайда

--- src/components/blocks/cache/cache-2.jsx
+++ src/components/blocks/cache/cache-2.jsx
@@ -115,6 +115,36 @@ const Cache2 = () => {
     return resultCache;
   };
 
+  /* 
+  Решение из гайда:
+  function rateLimit(logs) {
+    const encountered = new Map();
+    const rateLimited = [];
+
+    for (const {message, timestamp} of logs) {
+      const {timestamp: previousEncounterTimestamp, last: lastEncounter, occurences} = encountered.get(message) || {};
+
+      // если элемент произошёл впервые, либо сильно позже предыдущего
+      if (previousEncounterTimestamp === undefined || previousEncounterTimestamp < timestamp - 5) {
+        // по сообщению-ключу положим время встречи этого сообщения и место, где оно лежит
+        encountered.set(message, {timestamp, last: rateLimited.length, occurences: 1});
+
+        // и оставляем элемент в массиве
+        rateLimited.push({message, timestamp});
+      // а если мы такое уже встречали...
+      } else {
+        // то обновим время последней встречи
+        encountered.set(message, {timestamp, last: lastEncounter, occurences: occurences + 1});
+
+        // и обновим последнее подобное сообщение, чтобы отразить, что оно дублируется
+        rateLimited[lastEncounter].message = `${message} x${occurences + 1}`;
+      }
+    }
+
+    return rateLimited;
+  }
+  */
+
   return (
     <div>
       <h1>Ограничитель скорости обработки запросов системой логирования</h1>
