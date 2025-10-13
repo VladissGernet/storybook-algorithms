14d9eff0d8675790a4edc1e5aba707671319fe42
VladissGernet <skeimorr@gmail.com>
2025-10-13 10:21:25 +0400

    feat: начало рзабора задачи

---
+++ src/components/blocks/dynamic-programming/dynamic-programming.stories.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { StyledPage } from "/src/components/blocks/start/styled";
+
+import { DynamicProgramming1 } from "./dynamic-programming-1";
+
+export const backpack = () => (
+  <StyledPage>
+    <DynamicProgramming1 />
+  </StyledPage>
+);
+
+export default {
+  title: "Динамическое программирование",
+};
