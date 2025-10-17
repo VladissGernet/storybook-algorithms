c2cb9cafb978dafde243b7fde418848f5871f8e7
VladissGernet <skeimorr@gmail.com>
2025-10-17 15:26:43 +0400

    feat: добавил еще приер из гайда

--- src/components/blocks/dynamic-programming/dynamic-programming.stories.jsx
+++ src/components/blocks/dynamic-programming/dynamic-programming.stories.jsx
@@ -3,10 +3,17 @@ import React from "react";
 import { StyledPage } from "/src/components/blocks/start/styled";
 
 import { DynamicProgramming1 } from "./dynamic-programming-1";
+import { DynamicProgramming2 } from "./dynamic-programming-2";
 
 export const backpack = () => (
   <StyledPage>
     <DynamicProgramming1 />
+  </StyledPage>
+);
+
+export const maxIncome = () => (
+  <StyledPage>
+    <DynamicProgramming2 />
   </StyledPage>
 );
 
